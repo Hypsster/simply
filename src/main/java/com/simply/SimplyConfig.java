41dc067dcc9607b3e7116d7f6073bb8120309716
Hypsster <work@hypsster.com>
2023-03-08 18:57:36 -0500

    Updated Main Class

--- src/main/java/com/simply/SimplyConfig.java
+++ src/main/java/com/simply/SimplyConfig.java
@@ -1,10 +1,25 @@
 package com.simply;
 
+import com.google.inject.Provides;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigObject;
 
-@ConfigGroup("example")
+@ConfigGroup("simply")
 public interface SimplyConfig extends Config
 {
+
+   @ConfigItem(
+           keyName = "HP Threshold",
+           name = "HP Threshold",
+           description = "Enter the HP cut off to be warned."
+   )
+   default int hpThresh()
+   {
+      return 20;
+   }
+
+
 
 }
--- src/main/java/com/simply/SimplyPlugin.java
+++ src/main/java/com/simply/SimplyPlugin.java
@@ -1,13 +1,56 @@
 package com.simply;
 
+import com.google.inject.Inject;
+import com.google.inject.Provides;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.Skill;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.eventbus.EventBus;
 
 @Slf4j
 @PluginDescriptor(name = "Simply")
 
 public class SimplyPlugin extends Plugin
 {
+    @Inject
+    Client client;
+
+    @Inject
+    SimplyConfig config;
+
+    @Subscribe
+    private void onGameTick(GameTick event)
+    {
+        int hitpointsTracker = client.getBoostedSkillLevel(Skill.HITPOINTS); // Stores HP Level in variable
+
+        if (hitpointsTracker <= config.hpThresh())
+        {
+            // Run this code
+        }
+
+    }
+    @Subscribe
+    private void onGameStateChanged(GameStateChanged gameStateChanged)
+    {
+        if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+        {
+            client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Welcome to runscape", null);
+        }
+    }
+
+    @Provides
+    SimplyConfig provideConfig(ConfigManager configManager)
+    {
+        return configManager.getConfig(SimplyConfig.class);
+    }
+
 
 }
